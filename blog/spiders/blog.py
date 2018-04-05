39f49fceb46327930065109b2a98c8f3b50b2a04
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2018-04-05 21:59:48 +0800

    change size

--- blog/spiders/blog.py
+++ blog/spiders/blog.py
@@ -10,8 +10,9 @@ class BlogSpider(scrapy.Spider):
         super(BlogSpider, self).__init__(*args, **kwargs)
         self.start_urls = ["http://www.cnblogs.com/%s/default.html?page=1" % user]
         #http://www.cnblogs.com/mvc/blog/ViewCountCommentCout.aspx?postId=7416411
-        #http://www.cnblogs.com/mvc/blog/GetComments.aspx?postId=7416411&blogApp=zhaojiedi1992&pageIndex=0&anchorCommentId=0&_=1503640681602
+        #http://www.cnblogs.com/mvc/blog/GetComments.aspx?postId=7416411&blogApp=zhaojiedi1992&pageIndex=0&anchorCommentId=0
         self.user=user
+        self.Items=[]
     def parse(self, response):
         lis=response.css(".postTitle a::attr(href)").extract()
         for li in lis:
@@ -31,4 +32,28 @@ class BlogSpider(scrapy.Spider):
         result["date"]=response.css("#post-date::text").extract_first()
         result["read"]=response.css("#post_view_count::text").extract_first()
         result["say"]=response.css("#post_comment_count::text").extract_first()
-        yield  result
+        #http://i.cnblogs.com/EditPosts.aspx?postid=7416411
+        result["postid"]=response.css(".postDesc a[rel]::attr(href)").extract_first()[-7:]
+        result["readurl"]=response.css(".postDesc a[rel]::attr(href)").extract_first()
+        result["sayurl"] = response.css(".postDesc a[rel]::attr(href)").extract_first()
+        self.Items.append(result)
+        yield scrapy.Request("http://www.cnblogs.com/mvc/blog/ViewCountCommentCout.aspx?postId=%s" % result["postid"],callback=self.parse_item_read)
+        yield scrapy.Request("http://www.cnblogs.com/mvc/blog/GetComments.aspx?postId=%s&blogApp=%s&pageIndex=0&anchorCommentId=0" % result["postid"],self.user ,callback=self.parse_item_say)
+    def parse_item_read(self,response):
+        readurl=response.url
+        elems = [ x for x in self.Items if x["readurl"]==readurl]
+        if len(elems)==1:
+            elem=elems[0]
+            elem["read"]=response.css("body::text").extract_first()
+        else:
+            pass
+    def parse_item_say(self,response):
+        readurl=response.url
+        elems = [ x for x in self.Items if x["say"]==readurl]
+        if len(elems)==1:
+            elem=elems[0]
+            elem["read"]=response.css("body::text").extract_first()
+        else:
+            pass
+
+
